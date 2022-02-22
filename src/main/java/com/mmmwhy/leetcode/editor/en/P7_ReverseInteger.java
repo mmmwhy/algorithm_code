e2a20a8974a7bbc144fb5537a5ec612d76fe9de9
mmmwhy <mmmwhy@mail.ustc.edu.cn>
2022-02-22 21:29:41 +0800

    P6_ZigzagConversion

---
+++ src/main/java/com/mmmwhy/leetcode/editor/en/P7_ReverseInteger.java
@@ -0,0 +1,53 @@
+// Given a signed 32-bit integer x, return x with its digits reversed. If
+// reversing x causes the value to go outside the signed 32-bit integer range [-2³¹, 2³¹ -
+// 1], then return 0.
+//
+// Assume the environment does not allow you to store 64-bit integers (signed
+// or unsigned).
+//
+//
+// Example 1:
+//
+//
+// Input: x = 123
+// Output: 321
+//
+//
+// Example 2:
+//
+//
+// Input: x = -123
+// Output: -321
+//
+//
+// Example 3:
+//
+//
+// Input: x = 120
+// Output: 21
+//
+//
+//
+// Constraints:
+//
+//
+// -2³¹ <= x <= 2³¹ - 1
+//
+// Related Topics Math 👍 6630 👎 9320
+
+package com.mmmwhy.leetcode.editor.en;
+
+public class P7_ReverseInteger {
+  public static void main(String[] args) {
+    Solution solution = new P7_ReverseInteger().new Solution();
+  }
+
+  // leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+    public int reverse(int x) {
+      return 123;
+    }
+  }
+  // leetcode submit region end(Prohibit modification and deletion)
+
+}
