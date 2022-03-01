10375622dfda82f6af7801481e1f9f3e7dddfa9e
mmmwhy <mmmwhy@mail.ustc.edu.cn>
2022-03-02 06:46:16 +0800

    P11_ContainerWithMostWater

---
+++ src/main/java/com/mmmwhy/leetcode/editor/en/P11_ContainerWithMostWater.java
@@ -0,0 +1,57 @@
+//You are given an integer array height of length n. There are n vertical lines 
+//drawn such that the two endpoints of the iᵗʰ line are (i, 0) and (i, height[i]).
+// 
+//
+// Find two lines that together with the x-axis form a container, such that the 
+//container contains the most water. 
+//
+// Return the maximum amount of water a container can store. 
+//
+// Notice that you may not slant the container. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: height = [1,8,6,2,5,4,8,3,7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,
+//3,7]. In this case, the max area of water (blue section) the container can 
+//contain is 49.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: height = [1,1]
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// n == height.length 
+// 2 <= n <= 10⁵ 
+// 0 <= height[i] <= 10⁴ 
+// 
+// Related Topics Array Two Pointers Greedy 👍 14656 👎 878
+
+package com.mmmwhy.leetcode.editor.en;
+
+
+public class P11_ContainerWithMostWater{
+  public static void main(String[] args) {
+       Solution solution = new P11_ContainerWithMostWater().new Solution();
+  }
+  
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxArea(int[] height) {
+        
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
