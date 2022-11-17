844cfa20af1f58b194abae97fbd592771599f5c0
lifeiyang <lifeiyang@zhihu.com>
2022-11-17 21:59:29 +0800

    2022-11-17-06:11:26

--- leetcode/editor/cn/[14]最长公共前缀.py
+++ leetcode/editor/cn/[14]最长公共前缀.py
@@ -1,4 +1,34 @@
+from typing import List
+
+
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 1:
+            return strs[0]
+        
+        min_length = 200
+        for sub_str in strs:
+            min_length = min(len(sub_str), min_length)
+        
+        idx = 1
+        while idx <= min_length:
+            flag = True
+            for sub_str in strs[1:]:
+                if sub_str[:idx] != strs[0][:idx]:
+                    flag = False
+                    break
+            if not flag:
+                break
+            else:
+                idx += 1
+        return strs[0][:idx - 1]
+
+
 # leetcode submit region end(Prohibit modification and deletion)
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.longestCommonPrefix(["ab", "a"]))
+    print(solution.longestCommonPrefix(["flower", "flow", "flight"]))
+    print(solution.longestCommonPrefix(["dog", "racecar", "car"]))
--- leetcode/editor/cn/[516]最长回文子序列.py
+++ leetcode/editor/cn/[516]最长回文子序列.py
@@ -2,5 +2,25 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         # s[i:j] 中的最长回文子序列的长度是 dp[i][j]
+        length = len(s)
+        dp = [[0 for _ in range(length)] for _ in range(length)]
         
+        # i 和 j 位置相同的时候为 1
+        for i in range(length):
+            dp[i][i] = 1
+        for i in range(length - 1, -1, -1):
+            for j in range(i + 1, length):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        
+        return dp[0][length - 1]
+
+
 # leetcode submit region end(Prohibit modification and deletion)
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.longestPalindromeSubseq("cbbd"))
+    print(solution.longestPalindromeSubseq("bbbab"))
